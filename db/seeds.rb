07f3d40c8d2e44e7a4a4da332ebb6ee0acafd32c
yurimoon <sky.ac3ce@gmail.com>
2017-02-01 06:33:38 +0000

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,9 @@
 100.times do |n|
   email = Faker::Internet.email
   password = "password"
-  User.create!(email: email,
+  name = Faker::Internet.name
+  User.create!(name: name,
+               email: email,
                password: password,
                password_confirmation: password,
                )
